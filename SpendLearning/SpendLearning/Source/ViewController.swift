8d62eb884fb8b8d377c874bf0fe1555a988a8006
snughnu <ksnughnu@gmail.com>
2026-07-01 10:13:55 +0900

    chore: 프로젝트 생성 및 세팅

---
+++ SpendLearning/SpendLearning/Source/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  SpendLearning
+//
+//  Created by 김성훈 on 7/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
