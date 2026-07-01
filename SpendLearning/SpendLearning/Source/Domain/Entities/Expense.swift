99ee7af631693a39925c1d34ffd0e70be3adfc59
snughnu <ksnughnu@gmail.com>
2026-07-01 17:47:06 +0900

    feat: 도메인 엔티티 추가

---
+++ SpendLearning/SpendLearning/Source/Domain/Entities/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  SpendLearning
+//
+//  Created by 김성훈 on 7/1/26.
+//
+
+import Foundation
+
+struct Expense {
+    let date: Date?
+    let category: Category
+    let memo: String?
+    let amount: Int
+}
