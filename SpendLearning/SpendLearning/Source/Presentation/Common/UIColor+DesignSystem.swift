a1c9fe076ebeac74f5805d569c8bb38a22a057ed
snughnu <ksnughnu@gmail.com>
2026-07-08 10:01:09 +0900

    fix: 컬러셋 수정

--- SpendLearning/SpendLearning/Source/Presentation/Common/UIColor+DesignSystem.swift
+++ SpendLearning/SpendLearning/Source/Presentation/Common/UIColor+DesignSystem.swift
@@ -9,26 +9,26 @@ import UIKit
 
 extension UIColor {
     enum DesignSystem {
-        /// Primary - 딥 브라운 네이비
-        static let primary = UIColor(red: 0.18, green: 0.14, blue: 0.10, alpha: 1.0)
+        /// Primary - 딥 올리브 다크
+        static let primary = UIColor(red: 0.18, green: 0.21, blue: 0.15, alpha: 1.0) // #2E3526
 
-        /// Background - 흰색 베이지
-        static let background = UIColor(red: 0.98, green: 0.97, blue: 0.96, alpha: 1.0)
+        /// Background - 크림 베이지 (올리브 톤)
+        static let background = UIColor(red: 0.95, green: 0.95, blue: 0.92, alpha: 1.0) // #F2F2EB
 
-        /// Surface - 흰색 (카드 배경)
-        static let surface = UIColor.white
+        /// Surface - 연한 크림 화이트
+        static let surface = UIColor(red: 0.98, green: 0.98, blue: 0.96, alpha: 1.0) // #FAFAF5
 
-        /// Secondary - 연한 베이지 (아이콘 배경)
-        static let secondary = UIColor(red: 0.949, green: 0.898, blue: 0.867, alpha: 1.0)
+        /// Secondary - 연한 올리브 베이지 (아이콘 배경)
+        static let secondary = UIColor(red: 0.85, green: 0.87, blue: 0.82, alpha: 1.0) // #D9DED1
 
-        /// Accent - 포인트 컬러
-        static let accent = UIColor(red: 0.898, green: 0.384, blue: 0.118, alpha: 1.0)
+        /// Accent - 국방색 포인트
+        static let accent = UIColor(red: 0.325, green: 0.388, blue: 0.286, alpha: 1.0) // #536349
 
-        /// Subtitle - 중간 회색
-        static let subtitle = UIColor(red: 0.50, green: 0.48, blue: 0.46, alpha: 1.0)
+        /// Subtitle - 올리브 그레이
+        static let subtitle = UIColor(red: 0.45, green: 0.48, blue: 0.42, alpha: 1.0) // #737A6B
 
-        /// Separator - 연한 구분선
-        static let separator = UIColor(red: 0.91, green: 0.90, blue: 0.88, alpha: 1.0)
+        /// Separator - 연한 올리브 구분선
+        static let separator = UIColor(red: 0.87, green: 0.88, blue: 0.85, alpha: 1.0) // #DEE1D9
     }
 }
 
