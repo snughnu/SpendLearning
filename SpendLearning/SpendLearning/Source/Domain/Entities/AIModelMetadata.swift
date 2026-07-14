8e8eca0f732fba3c58b2b0aded8ad0112698fb5b
snughnu <ksnughnu@gmail.com>
2026-07-14 14:21:48 +0900

    feat: AIModelMetadata 엔티티 생성

---
+++ SpendLearning/SpendLearning/Source/Domain/Entities/AIModelMetadata.swift
@@ -0,0 +1,15 @@
+//
+//  AIModelMetadata.swift
+//  SpendLearning
+//
+//  Created by 김성훈 on 7/14/26.
+//
+
+import Foundation
+
+struct AIModelMetadata {
+    let id: String
+    let dataCount: Int
+    let accuracy: Float
+    let createdAt: Date
+}
