31f6404996969ad735c6dbbdf21a6421507de646
snughnu <ksnughnu@gmail.com>
2026-07-14 21:07:59 +0900

    feat: PredictionStrategy 구현

---
+++ SpendLearning/SpendLearning/Source/Data/Strategies/PredictionStrategy.swift
@@ -0,0 +1,15 @@
+//
+//  PredictionStrategy.swift
+//  SpendLearning
+//
+//  Created by 김성훈 on 7/14/26.
+//
+
+protocol PredictionStrategy {
+    /// 소비 패턴 기반 인사이트 목록
+    func predictInsights(expenses: [Expense]) -> [AIInsightItem]
+    /// 일별 예측 지출 금액 [일: 예측금액]
+    func predictDaily(expenses: [Expense], year: Int, month: Int) -> [Int: Int]
+    /// 카테고리별 예측 지출 금액 [카테고리명: 예측금액]
+    func predictCategory(expenses: [Expense], year: Int, month: Int) -> [String: Int]
+}
